aa3d682495198c5482b92d1c45ca0b24e2415e68
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-18 00:06:09 +0530

    Added:Feature For Deleting Contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,36 +1,107 @@
 package com.bridgelabz;
+import java.util.Arrays;
 import java.util.Scanner;
 public class AddressBook {
 	private static Scanner scanner = new Scanner(System.in);
+	private static Contact[] contacts = new Contact[3];
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book");
-		Contact contact = addContact();
-		System.out.println(contact);
+		boolean isExit = false;
+		while (!isExit) {
+			System.out.println("Please select options\n1. add contact\n2. edit contact\n3. delete contact\n4. exit");
+			int choice = Integer.parseInt(scanner.nextLine());
+			switch (choice) {
+			case 1:
+				addContact();
+				System.out.println(Arrays.toString(contacts));
+				break;
+			case 2:
+				System.out.println("Enter name to edit");
+				String name = scanner.nextLine();
+				int contactIndex = findContactByName(name);
+				System.out.println(contactIndex);
+				if (contactIndex == -1) {
+					System.out.println("Contact not found with name: " + name);
+				} else {
+					Contact contact = editContact(contacts[contactIndex]);
+					contacts[contactIndex] = contact;
+				}
+				System.out.println(Arrays.toString(contacts));
+				break;
+			case 3:
+				System.out.println("Enter name to delete");
+				name = scanner.nextLine();
+				int contactIndexForDelete = findContactByName(name);
+				if (contactIndexForDelete == -1) {
+					System.out.println("Contact not found with name: " + name);
+				} else {
+					contacts[contactIndexForDelete] = null;
+				}
+				System.out.println(Arrays.toString(contacts));
+				break;
+			case 4:
+				isExit = true;
+				System.out.println("Exiting...");
+				break;
+			default:
+				System.out.println("Please select valid input");
+			}
+		}
+		
 	}
 
 	private static Contact addContact() {
 		Contact contact = new Contact();
-		System.out.println("Enter First Name:");
-		contact.setFirstName(scanner.next());
+		System.out.println("Enter first name:");
+		contact.setFirstName(scanner.nextLine());
+
 		System.out.println("Enter last name:");
-		contact.setLastName(scanner.next());
+		contact.setLastName(scanner.nextLine());
+
 		System.out.println("Enter address:");
-		contact.setAddress(scanner.next());
-		System.out.println("Enter City:");
-		contact.setcity(scanner.next());
-		System.out.println("Enter State:");
-		contact.setstate(scanner.next());
-		System.out.println("Enter Zip:");
-		contact.setzip(scanner.next());
-		System.out.println("Enter Phone Number:");
-		contact.setphoneNumber(scanner.next());
-		System.out.println("Enter email:");
-		contact.setemail(scanner.next());
-		System.out.println();
-		System.out.println(contact);
-		System.out.println();
-		System.out.println("Edit First name:");
-		contact.setFirstName(scanner.next());
+		contact.setAddress(scanner.nextLine());
+
+		System.out.println("Enter city :");
+		contact.setcity(scanner.nextLine());
+
+		System.out.println("Enter state :");
+		contact.setstate(scanner.nextLine());
+
+		System.out.println("Enter zip :");
+		contact.setzip(scanner.nextLine());
+
+		System.out.println("Enter phoneNumber :");
+		contact.setphoneNumber(scanner.nextLine());
+
+		System.out.println("Enter email :");
+		contact.setemail(scanner.nextLine());
+
+		for (int i = 0; i < contacts.length; i++) {
+			if (contacts[i] == null) {
+				contacts[i] = contact;
+				System.out.println("Contact added successfully");
+				return contact;
+			}
+		}
+		System.out.println("No space for new contact");
 		return contact;
+	}
+
+	private static Contact editContact(Contact contact) {
+		System.out.println("Edit first name");
+		contact.setFirstName(scanner.nextLine());
+		return contact;
+	}
+
+	private static int findContactByName(String name) {
+		for (int i = 0; i < contacts.length; i++) {
+			if (contacts[i] != null) {
+				if (contacts[i].getFirstName().equals(name) ||
+						contacts[i].getLastName().equals(name)) {
+					return i;
+				}
+			}
+		}
+		return -1;
 	}
 }
