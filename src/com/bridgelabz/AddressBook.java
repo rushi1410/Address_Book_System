87bd9b6b0925e862adc6aeb1e6c901f220e0bfe9
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-17 18:16:15 +0530

    Added:Create Contact Of Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,7 +1,45 @@
 package com.bridgelabz;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class AddressBook {
-public static void main(String[] args) {
-	System.out.println("Welcome To Address Book Program");
-}
+	 public static void main(String[] args) {
+	        List<String> person = new ArrayList<String>();
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.println("Welcome to Address Book System");
+
+	        System.out.println("Enter First name: ");
+	        String fname = input.nextLine();
+
+	        System.out.println("Enter Last name: ");
+	        String lname = input.nextLine();
+
+	        System.out.println("Enter First Address: ");
+	        String address = input.nextLine();
+
+	        System.out.println("Enter City: ");
+	        String city = input.nextLine();
+
+	        System.out.println("Enter State: ");
+	        String state = input.nextLine();
+
+	        System.out.println("Enter Zip: ");
+	        String zip = input.nextLine();
+
+	        System.out.println("Enter Phone number: ");
+	        String phone = input.nextLine();
+
+	        person.add(fname);
+	        person.add(lname);
+	        person.add(address);
+	        person.add(city);
+	        person.add(state);
+	        person.add(zip);
+	        person.add(phone);
+
+	        for (String str : person){
+	            System.out.println(str);
+	        }
+	    }
 }
